0149fadbb24baf1b3b309a71d3d23b1b90e0a4ac
SebellKo <fulfilled_@naver.com>
2023-01-03 04:47:25 +0900

    home img

---
+++ index.js
@@ -0,0 +1,14 @@
+const homeHeart = document.querySelector('#homeHeart');
+const homeWater = document.querySelector('#homeWater');
+const homePill = document.querySelector('#homePill');
+const homeCross = document.querySelector('#homeCross');
+const home = document.querySelector('#home');
+
+window.addEventListener('load', () => {
+    homeHeart.style.transform = 'translateX(0%) translateY(0%) scale(1.3)';
+    homeWater.style.transform = 'translateX(0%) translateY(0%) scale(1.3)';
+
+    homePill.style.transform = 'translateX(0%) translateY(0%) scale(1.3)';
+    homeCross.style.transform = 'translateX(0%) translateY(0%) scale(1.3)';
+})
+
